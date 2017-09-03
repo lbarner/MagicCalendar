48b39598ec2d859db9c23b4df6c7b2377489d61f
Lyle Barner <lyle.barner@gmail.com>
2017-09-03 19:11:36 +0000

    Remove API key from repo

--- Weather/get_weather.py
+++ Weather/get_weather.py
@@ -18,9 +18,19 @@ def parse_current_conditions_wunderground(json_data, value):
 	
 	return return_value
 
-def get_current_conditions_wunderground(api_key, city, state):
+def get_current_conditions_wunderground(key_path, city, state):
 	# Initialize the variables
 	json_response = None	
+
+	# Import the public key
+	key_fh = open(key_path, 'r')
+	key_data = key_fh.readlines()
+	key_fh.close()
+	for line in key_data:
+		if 'wunderground' in line: 
+			api_key = re.split('=', line)[-1].strip()
+
+	print(api_key)
 
 	# Create the target url
 	target_url = 'http://api.wunderground.com/api/' + api_key + '/conditions/q/' + state + '/' + city + '.json'
@@ -31,12 +41,12 @@ def get_current_conditions_wunderground(api_key, city, state):
 	return json_response
 
 if __name__ == '__main__':
-	wunderground_key = '887e3ed0a66ad9c9'
+	key_path = '/home/pi/APIKeys'
 	city = 'Pasadena'
 	state = 'CA'
 
 	# Make the call to the api
-	json_data = get_current_conditions_wunderground(wunderground_key, city, state)
+	json_data = get_current_conditions_wunderground(key_path, city, state)
 
 	# Parse the response
 	current_temp = parse_current_conditions_wunderground(json_data, 'temp')
